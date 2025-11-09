1518f984088c6ecfa2cfc5f93141c6b57b90d061
minh10m <minhhh1010@gmail.com>
2025-11-09 09:36:54 +0700

    fix(user): edit function favorites

--- src/main/java/com/minh/Online/Food/Ordering/modules/user/UserRepository.java
+++ src/main/java/com/minh/Online/Food/Ordering/modules/user/UserRepository.java
@@ -1,6 +1,8 @@
 package com.minh.Online.Food.Ordering.modules.user;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +18,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query("update User u set u.password = ?2 where u.email = ?1")
     void updatePassword(String email, String password);
+
+    @EntityGraph(attributePaths = "favorites")
+    Optional<User> findById(Long id);
+    @Query("select u from User u left join fetch u.favorites where u.id = :id")
+    Optional<User> findByIdWithFavorites(@Param("id") Long id);
 }
